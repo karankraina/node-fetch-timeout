0ba9eaec709f1e20e8da5ef681f22ecec9c59f12
Karan Raina <karanraina1996@gmail.com>
2021-12-17 00:12:43 +0530

    feature: added fetch method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-export default () => {
-    console.log(' This is working ')
-}
+import fetch from 'node-fetch';
+import 'babel-polyfill';
+
+/**
+ * Make http/ https request with timeout and retries
+ * 
+ * @param {httpOptions} options Standard options for http.request
+ * @param {number} timeout timeout in milliseconds
+ * @param {number} retryCount number of retries
+ * @returns http response
+ */
+export default async (url, options) => {
+    const { retryCount = 1, ...requestOptions } = options;
+    let currentRetryCount = 1;
+    while (currentRetryCount <= retryCount) {
+        try {
+            const response = await fetch(url, requestOptions);
+            // return raw response
+            return response;
+        } catch (error) {
+            // return in last iteration
+            if (currentRetryCount === retryCount) return error;
+            currentRetryCount++;
+        }
+    }
+};
